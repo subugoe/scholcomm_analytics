e2529e0d3e110708e01f0cddf5e240aaa737b146
Anne Hobert <ahein1@sub.uni-goettingen.de>
2019-04-30 00:21:42 +0200

    proof reading

--- _posts/unpaywall_evidence/database/evidence_single_cat_08_12.sql
+++ _posts/unpaywall_evidence/database/evidence_single_cat_08_12.sql
@@ -1,11 +1,9 @@
 SELECT
         ev_cat,
-        year,
         COUNT(distinct(doi)) AS number_of_articles 
     FROM(
         SELECT
                 doi,
-                year,
                 STRING_AGG(DISTINCT(evidence), "&" ORDER BY evidence) AS ev_cat
             FROM
                 `oadoi_full.feb_19_mongo_export_2008_2012_full_all_genres`,
@@ -14,9 +12,7 @@ SELECT
                 genre = 'journal-article'
                 AND data_standard = 2
             GROUP BY
-                doi,
-                year
+                doi
     )
     GROUP BY
-        ev_cat,
-        year
+        ev_cat
