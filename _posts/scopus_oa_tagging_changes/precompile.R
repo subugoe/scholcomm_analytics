b8592c8eba5d20dfc667a06d55d192007b81592b
doerner12 <doerner@sub.uni-goettingen.de>
2025-01-23 14:04:23 +0100

    update scp_oa_tagging_changes

---
+++ _posts/scopus_oa_tagging_changes/precompile.R
@@ -0,0 +1,4 @@
+library(knitr)
+#spin("scopus_oa_tagging_changes.Rmd", "scopus_oa_tagging_changes.R")
+knitr::knit("_posts/scopus_oa_tagging_changes/scopus_oa_tagging_changes.Rmd.orig", output = "_posts/scopus_oa_tagging_changes/scopus_oa_tagging_changes.Rmd")
+
